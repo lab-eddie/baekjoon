7ed02d5cf0fd1369ce2f742267d15dd335a6e0d4
eddie <popokbs@gmail.com>
2022-09-07 23:59:54 +0900

    baekjoon

---
+++ backtracking.py
@@ -0,0 +1,16 @@
+def make_arr(k, n, m):
+    if k == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] = True
+            arr.append(i+1)
+            make_arr(k+1, n, m)
+            visited[i] = False
+            arr.pop()
+            
+n, m = map(int, input().split())
+visited = [False for _ in range(n)]
+arr = []
+make_arr(0, n, m)
--- text.py
+++ text.py
@@ -1,19 +1,13 @@
 n = int(input())
 
-arr = ["1","2","3"]
-
-while n > 0:
-    a, b = map(str, input().split())
+for i in range(n):
+    ps = [_ for _ in input()]
     
-    if 0 > int(a) > 3 or 0 > int(b) > 3:
-        print(-1)
-        quit()
+    l = 0
+    
+    for j in ps:
+        if ps[0] == ")":
+            print("NO")
+            break
         
-    tmp1 = arr.index(a)
-    tmp2 = arr.index(b)
-    arr[tmp1] = b
-    arr[tmp2] = a
-    
-    n -= 1
-
-print(arr[0])
+        elif 

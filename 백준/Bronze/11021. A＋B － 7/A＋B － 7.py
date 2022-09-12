418295242d9fee672b4a7b205b66b2d71f29a262
lab-eddie <108397009+lab-eddie@users.noreply.github.com>
2022-09-12 22:28:27 +0900

    [Bronze V] Title: A+B - 7, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,4 +1,7 @@
-t=int(input())
-for i in range(1,t+1):
-    a,b = map(int,input().split())
-    print(f"Case #{i}: {a+b}")
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(f"Case #{i+1}: {a+b}")

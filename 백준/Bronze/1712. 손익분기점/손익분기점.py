17b685036024fc23d4a849802c774a48f41df932
lab-eddie <108397009+lab-eddie@users.noreply.github.com>
2022-09-27 01:34:58 +0900

    [Bronze II] Title: 손익분기점, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.py
+++ 백준/Bronze/1712. 손익분기점/손익분기점.py
@@ -1,9 +1,6 @@
-import math
-fix, vari, price = map(int,input().split())
-if vari >= price :
+a, b, c = map(int,input().split())
+
+if c - b <= 0:
     print(-1)
-    quit()
-bep = fix / (price-vari)
-if fix % (price - vari) == 0:
-    print(round(bep + 1))
-else : print(math.ceil(bep))
+else :
+    print(a // (c - b) + 1)

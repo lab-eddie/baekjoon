7695b43f699a158e4731c93f9b6fbce040d26599
lab-eddie <108397009+lab-eddie@users.noreply.github.com>
2022-10-09 23:52:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1408 ms, Memory: 75672 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,10 +1,13 @@
 import sys
+input = sys.stdin.readline
 
 n = int(input())
+
 arr = []
 for i in range(n):
-    arr.append(int(sys.stdin.readline()))
+    arr.append(int(input()))
 
 arr.sort()
-for j in arr:    
+
+for j in arr:
     print(j)

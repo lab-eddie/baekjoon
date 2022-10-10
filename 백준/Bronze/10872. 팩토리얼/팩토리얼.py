588a55ac7c9002472f5e2f19d7bbb21e7c1f44d2
lab-eddie <108397009+lab-eddie@users.noreply.github.com>
2022-10-10 22:26:19 +0900

    [Bronze V] Title: 팩토리얼, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,3 +1,11 @@
-import math
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-print(math.factorial(n))
+
+result = 1
+while n != 0:
+    result = result * n
+    n -= 1
+    
+print(result)
